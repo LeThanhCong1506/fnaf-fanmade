31b7f9468283ee0a8c5057fd19d1f4e0351a6d1c
LeThanhCong1506 <154421089+LeThanhCong1506@users.noreply.github.com>
2026-01-02 16:34:23 +0700

    Cong has done task
    [refactor]
    ui features

--- Assets/Scripts/GameLogic/Player/Flashlight.cs
+++ Assets/Scripts/GameLogic/Player/Flashlight.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86d1f14bd20875a201c72bfd8e3f379e1f2ef8a6002b99555eea798467a3bf72
-size 2332
+oid sha256:d80ffc98b25d29afe9ab441e083a1c825df24eedbff43b868954af7f90f05f53
+size 2639
