30d7129d4c895342f09c44912530893d85a15e94
LeThanhCong1506 <154421089+LeThanhCong1506@users.noreply.github.com>
2026-01-16 15:58:52 +0700

    Cong has done task
    [fix]

--- Assets/Scripts/UI/Widgets/InScreens/InGame/MissionUI.cs
+++ Assets/Scripts/UI/Widgets/InScreens/InGame/MissionUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2a084205e601dd8a539f9260f48573295295c09eb3364c433733f2458a3ddfe
-size 3433
+oid sha256:faa6525df778f2770d24458665d5b2abfa860466c15eb419051c7f7bcbfeaa62
+size 3279
