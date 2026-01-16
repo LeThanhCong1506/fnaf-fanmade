a02887d604d6f3df19688f6f46362ab6b8f745d2
LeThanhCong1506 <154421089+LeThanhCong1506@users.noreply.github.com>
2026-01-16 17:55:33 +0700

    Cong has done task
    [update]
    update menus

--- Assets/Scripts/UI/Popups/MiniMenu.cs
+++ Assets/Scripts/UI/Popups/MiniMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c97a3656bde436125fbc1d7e8adb6717814eb1b32f39a62d8f8d6e61586d711
-size 2710
+oid sha256:917a15f29ab263eb9f2e1a9c55497ac58e296f7ec73ec5d69e3334514ac943bc
+size 2836
